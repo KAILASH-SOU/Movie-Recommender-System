1abc6f04331bdfe7d2c95b6112984916096640de
KAILASH <145152589+KAILASH-SOU@users.noreply.github.com>
2025-07-26 12:00:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,23 @@
 import pickle
 import streamlit as st
-import gdown
 import os
-# ========== Google Drive File IDs ==========
-movie_file_id = '1-D0Xe33hJgFUWRn6mKFUTelaXv57Pscf'
-similarity_file_id = '194CMNVqbv8t-hzxgkOK3qRg4tV69mTbk'
-
-# ========== Download Pickle Files if Not Present ==========
-if not os.path.exists('movie_list.pkl'):
-    gdown.download(f'https://drive.google.com/uc?id={movie_file_id}', 'movie_list.pkl', quiet=False)
-
-if not os.path.exists('similarity.pkl'):
-    gdown.download(f'https://drive.google.com/uc?id={similarity_file_id}', 'similarity.pkl', quiet=False)
-
 
 # ========== Load Pickle Files ==========
-movies = pickle.load(open('movie_list.pkl', 'rb'))
-similarity = pickle.load(open('similarity.pkl', 'rb'))
+@st.cache_data
+def load_data():
+    with open('movie_list.pkl', 'rb') as f:
+        movies = pickle.load(f)
+    with open('similarity.pkl', 'rb') as f:
+        similarity = pickle.load(f)
+    return movies, similarity
+
+movies, similarity = load_data()
 
 # ========== Recommendation Logic ==========
 def recommend(movie):
     index = movies[movies['title'] == movie].index[0]
     distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
-    recommended_movie_names = []
-
-    for i in distances[1:6]:
-        recommended_movie_names.append(movies.iloc[i[0]].title)
-
+    recommended_movie_names = [movies.iloc[i[0]].title for i in distances[1:6]]
     return recommended_movie_names
 
 # ========== Streamlit UI ==========
